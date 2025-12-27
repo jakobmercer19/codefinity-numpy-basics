b9f8c4d89fcf169f162bff3eeb24d550ddb7e847
jakobmercer19 <>
2025-12-27 22:36:45 +0000

    Automatic array creation

---
+++ num_py_basics/automatic_array_creation/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+# Create an array of even numbers from 2 to 21 exclusive
+even_numbers = 0.21(2, 4,  ___)
+# Create an array of 10 equally spaced numbers between 5 and 6 exclusive
+samples = ___.___(___, ___, ___, ___=___)
+print(even_numbers)
+print(samples)
